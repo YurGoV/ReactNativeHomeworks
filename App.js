3ddee8b3d5d45079c95ddac2ccaae157536f6ad2
YurGoV <yurgov@gmail.com>
2023-01-22 19:28:08 +0200

    AppLoading/haveError

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from "react";
+import * as Font from 'expo-font';
+// import {AppLoading} from 'expo';
+import AppLoading from 'expo-app-loading';
 
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    View,
+    TextInput,
+    TouchableWithoutFeedback, // імпорт компонента обгортки
+    Keyboard, // імпорт компонента клавіатури
+    KeyboardAvoidingView, // новий імпорт
+    Platform, // новий імпорт
+    Alert,
+    Button,
+} from "react-native";
+
+import {styles} from "./App.styles";
+
+
+const loadFonts = async () => {
+    await Font.loadAsync({
+        "Roboto-Regular": require("./img/fonts/Roboto-Regular.ttf"),
+        "Roboto-Bold": require("./img/fonts/Roboto-Bold.ttf"),
+    });
+};
+
+// const loadFonts = () => {
+//     return Font.loadAsync({
+//         "Roboto-Regular": require("./img/fonts/Roboto-Regular.ttf"),
+//         "Roboto-Bold": require("./img/fonts/Roboto-Bold.ttf"),
+//     });
+// };
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js YAHO-OO to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [isReady, setIsReady] = useState(false)
+
+    const nameHandler = (text) => setName(text);
+    const passwordHandler = (text) => setPassword(text);
+
+    const onLogin = () => {
+        Alert.alert("Credentials", `${name} + ${password}`);
+    };
+
+    if (!isReady) {
+        return <AppLoading startAsync={loadFonts}
+                           onFinish={() => setIsReady(true)}
+                           onError={(err) => console.log(err)}/>
+    }
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+                <KeyboardAvoidingView // визначаємо ОС та налаштовуємо поведінку клавіатури
+                    behavior={Platform.OS == "ios" ? "padding" : "height"}
+                >
+                    {/*<Text>Open up App.js YAHO-OO // YESS )to start working on your app!</Text>*/}
+
+                    <TextInput
+                        value={name}
+                        onChangeText={nameHandler}
+                        placeholder="Username"
+                        style={styles.input}
+                    />
+                    <TextInput
+                        value={password}
+                        onChangeText={passwordHandler}
+                        placeholder="Password"
+                        secureTextEntry={true}
+                        style={styles.input}
+                    />
+                    <Button title={"Login"} style={styles.input} onPress={onLogin}/>
+
+                    <StatusBar style="auto"/>
+                </KeyboardAvoidingView>
+            </View>
+        </TouchableWithoutFeedback>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ App.styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ecf0f1",
+    },
+    input: {
+        width: 200,
+        height: 44,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "black",
+        borderRadius: 5,
+        marginBottom: 10,
+    },
+});
