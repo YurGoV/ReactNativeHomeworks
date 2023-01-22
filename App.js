c7247a090ece6964057963ec5aa769dbabed08ca
YurGoV <yurgov@gmail.com>
2023-01-22 19:29:00 +0200

    ...

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from "react";
 import * as Font from 'expo-font';
-// import {AppLoading} from 'expo';
-import AppLoading from 'expo-app-loading';
+import {AppLoading} from 'expo';
+// import AppLoading from 'expo-app-loading';
 
 import {
     View,
