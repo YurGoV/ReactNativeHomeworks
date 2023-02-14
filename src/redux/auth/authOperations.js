f38865c1c10c617e9aeff5787f87e439369ebbe6
YurGoV <yurgov@gmail.com>
2023-02-14 15:08:42 +0200

    draft/ AsyncStorage_warning/resolved

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,12 +1,24 @@
 import db from '../../../firebase/config'
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
-// todo: https://amanhimself.dev/blog/remove-asyncstorage-has-been-extracted-warning-using-firebase/
+
+
+// todo: /DONE/ https://amanhimself.dev/blog/remove-asyncstorage-has-been-extracted-warning-using-firebase//
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+    initializeAuth,
+    getReactNativePersistence
+} from 'firebase/auth/react-native';
+
+const auth = initializeAuth(db, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
+
 
 export const authSignUpUser = ({email, password, login}) => (dispatch, getState) => {
     // try {
     // const user = await db.auth().createUserWithEmailAndPassword(email, password)
-    const auth = getAuth();
+    // const auth = getAuth();
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in
@@ -24,7 +36,10 @@ export const authSignUpUser = ({email, password, login}) => (dispatch, getState)
 export const authSignInUser = ({email, password}) => (dispatch, getState) => {
     // try {
     // const user = await db.auth().createUserWithEmailAndPassword(email, password)
-    const auth = getAuth();
+    // const auth = getAuth();
+
+
+
     signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in
